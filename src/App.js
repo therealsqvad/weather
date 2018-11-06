a67fc1caeaf88b0c38acce5c1d83b606b5efd23b
therealsqvad <nikita_klepov@mail.ru>
2018-11-06 11:19:44 +0700

    change api (yahoo), add icons

--- src/App.js
+++ src/App.js
@@ -19,16 +19,28 @@ render() {
   req.onreadystatechange = () => {
     if (req.readyState === 4) {
       if (req.status === 200) {
+        const weatherIconMap = [
+          'storm', 'storm', 'storm', 'lightning', 'lightning', 'snow', 'hail', 'hail',
+          'drizzle', 'drizzle', 'rain', 'rain', 'rain', 'snow', 'snow', 'snow', 'snow',
+          'hail', 'hail', 'fog', 'fog', 'fog', 'fog', 'wind', 'wind', 'snowflake',
+          'cloud', 'cloud_moon', 'cloud_sun', 'cloud_moon', 'cloud_sun', 'moon', 'sun',
+          'moon', 'sun', 'hail', 'sun', 'lightning', 'lightning', 'lightning', 'rain',
+          'snowflake', 'snowflake', 'snowflake', 'cloud', 'rain', 'snow', 'lightning'
+        ];
+
         const JSONstr = req.responseText;
-        const A = JSON.parse(JSONstr);
+        const forecast = JSON.parse(JSONstr);
+        const item = forecast.query.results.channel.item.condition;
 
-        console.log(A);
-        const tmp = A.list['1'].main.temp;
+        this.setState({ weatherIcon: `/icons/${weatherIconMap[item.code]}.png` });
 
-        this.setState({ temperature: tmp });
-        const city = A.list['1'].name;
+        console.log(forecast);
+        // const tmp = forecast.list['1'].main.temp;
 
-        document.getElementById('city').textContent = city;
+        this.setState({ temperature: item.temp });
+        // const city = forecast.list['1'].name;
+
+        document.getElementById('city').textContent = forecast.query.results.channel.location.city;
       }
     }
   };
@@ -38,8 +50,12 @@ render() {
     const lon = position.coords.longitude;
 
     this.setState({ latitude: lat, longitude: lon });
+    const DEG = 'c';
+    const wsql = `select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="(${lat},${lon})") and u="${DEG}"`;
+    const weatherYQL = `https://query.yahooapis.com/v1/public/yql?q=${encodeURIComponent(wsql)}&format=json&ttl=90`;
+    // req.open('GET', `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&type=like&APPID=cda95ddeb17adb2fc338e7fe7c132ab2`, true);
 
-    req.open('GET', `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&type=like&APPID=cda95ddeb17adb2fc338e7fe7c132ab2`, true);
+    req.open('GET', weatherYQL);
     req.send();
   });
   // req.open('GET', 'https://query.yahooapis.com/v1/public/yql?q=select+*+from+weather.forecast+where+woeid+in+(select+woeid+from+geo.places(1)+where+text%3D%27Novosibirsk+Russia%27)+AND+u%3D%27c%27&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&format=json', true);
@@ -51,13 +67,14 @@ render() {
   const { latitude } = this.state;
   const { longitude } = this.state;
   const { temperature } = this.state;
+  const { weatherIcon } = this.state;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div id="gradus">
-          {(temperature - 273.15).toFixed()}
+          {temperature}
             °
         </div>
         <div id="city" />
@@ -66,17 +83,19 @@ render() {
           <Marker lat={latitude} lon={longitude}>
             <MarkerLayout>
               <div style={{
-                borderRadius: '5%',
-                background: '#282c34',
+                borderRadius: '8px',
+                background: 'rgba(40, 44, 52, 0.3)',
                 shadow: '13px',
                 color: '#fff'
               }}
               >
-                <img src="https://img-android.lisisoft.com/imgmic/9/0/2909-i-info.spychalski.winguweather.jpg" width="16px" alt="none" />
-                <b>
-                  {(temperature - 273.15).toFixed()}
+                <img src={weatherIcon} width="40px" alt="none" id="weatherIcon" />
+                <span className="temperatura">
+                  <b>
+                    {temperature}
                     °
-                </b>
+                  </b>
+                </span>
               </div>
             </MarkerLayout>
           </Marker>
